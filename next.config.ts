3646b508366bc100772d15c12b31fade75ba4a7e
kimly.888 <kimly.jet@gmail.com>
2025-03-06 14:30:04 +0900

    Configure Next.js build output directory

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // If you're using the src directory pattern
+  distDir: '.next',
 };
 
 export default nextConfig;
